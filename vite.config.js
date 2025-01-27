2ecda6bf7327faac7d6659b2eb5ad31b6ae09265
JasirV <jasirmon133@gmail.com>
2025-01-27 14:08:34 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true, // Expose the server to the network
-  },
-  build: {
-    outDir: 'dist', // Specify the output directory
-    sourcemap: true, // Generate source maps for debugging
-    emptyOutDir: true, // Clean the output directory before building
-  },
-  resolve: {
-    alias: {
-      '@': '/src', // Alias for the src directory
-    },
-  },
-});
+})
